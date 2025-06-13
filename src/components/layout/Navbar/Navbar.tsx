c5250f4353227daa0758d021204c90aaaea42881
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 <sabaudianin@gmail.com>
2025-06-13 18:14:06 +0200

    added main Layout

---
+++ src/components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 shadow sticky top-0 z-50">
+      <h1 className="text-lg font-bold">🪣 BucketList</h1>
+    </header>
+  );
+};
