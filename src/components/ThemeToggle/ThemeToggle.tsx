514b7b9ddcfe64a9385b855899aa3d095fcce4a1
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 <sabaudianin@gmail.com>
2025-06-18 13:42:20 +0200

    refactored toogle to zustand

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -3,11 +3,11 @@ import { useTheme } from "../../hooks/useTheme/useTheme";
 
 export const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
+
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition"
-      aria-label="Change Theme"
+      className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300"
     >
       {theme === "dark" ? "🌙" : "☀️"}
     </button>
